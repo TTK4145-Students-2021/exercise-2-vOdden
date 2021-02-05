e54587797bdfce6b78dde5e0efe4a0b7639ee361
Sigurd <sigurdodden@gmail.com>
2021-02-05 13:46:25 +0100

    .go task

--- 2 - shared variable/go/foo.go
+++ 2 - shared variable/go/foo.go
@@ -7,13 +7,17 @@ import (
 	"runtime"
 )
 
-func number_server(add <-chan int, sub <-chan int, read chan<- int) {
+func number_server(add <-chan int, sub chan<- int, read <-chan int) {
 	var number = 0
 
-	// This for-select pattern is one you will become familiar with...
 	for {
 		select {
-        }
+		case num := <-add:
+			number += num
+		case sub <- number:
+		case <-read:
+			return
+		}
 	}
 }
 
@@ -21,25 +25,41 @@ func incrementer(add chan<- int, finished chan<- bool) {
 	for j := 0; j < 1000000; j++ {
 		add <- 1
 	}
-	//TODO: signal that the goroutine is finished
+	// Signal that the goroutine is finished
+	finished <- true
+
 }
 
 func decrementer(sub chan<- int, finished chan<- bool) {
-	for j := 0; j < 1000000+1; j++ {
-		sub <- 1
+	for j := 0; j < 1000000; j++ {
+		sub <- -1
 	}
-	//TODO: signal that the goroutine is finished
+	// Signal that the goroutine is finished
+	finished <- true
+
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// TODO: Construct the remaining channels
-	read := make(chan int)
+	// Construct the required channels
+	CH_add := make(chan int)
+	CH_sub := make(chan int)
+	CH_read := make(chan int)
 
-	// TODO: Spawn the required goroutines
+	CH_incr_finished := make(chan bool)
+	CH_decr_finished := make(chan bool)
 
-	// TODO: block on finished from both "worker" goroutines
 
-	fmt.Println("The magic number is:", <-read)
+	// Spawn the required goroutines
+	go number_server(CH_add, CH_sub, CH_read)
+	go incrementer(CH_add,CH_incr_finished)
+	go decrementer(CH_add,CH_decr_finished)
+
+	// Block on finished from both "worker" goroutines
+	<- CH_incr_finished
+	<- CH_decr_finished
+
+	fmt.Println("The magic number is:", <-CH_sub)
+	CH_read <- 0
 }
